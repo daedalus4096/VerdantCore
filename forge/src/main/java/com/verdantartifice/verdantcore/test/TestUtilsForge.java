977c8829ee6b1c16ed30af8e4e3b29d1c081dd9e
Michael Bunting <michael.a.bunting@gmail.com>
2025-05-29 12:59:21 -0700

    Add canary tests

---
+++ forge/src/main/java/com/verdantartifice/verdantcore/test/TestUtilsForge.java
@@ -0,0 +1,5 @@
+package com.verdantartifice.verdantcore.test;
+
+public class TestUtilsForge {
+    public static final String DEFAULT_TEMPLATE = "verdantcore:test/empty3x3x3";
+}
---
+++ neoforge/src/main/java/com/verdantartifice/verdantcore/test/TestUtilsNeoforge.java
@@ -0,0 +1,8 @@
+package com.verdantartifice.verdantcore.test;
+
+import com.verdantartifice.verdantcore.Constants;
+
+public class TestUtilsNeoforge {
+    public static final String DEFAULT_TEMPLATE = "test/empty3x3x3";
+    public static final String DEFAULT_GENERATOR_TEMPLATE = String.join(":", Constants.MOD_ID, DEFAULT_TEMPLATE);
+}
